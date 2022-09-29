f46229fdfb38356fe39922267da255e0fbe6de4c
EMEAAD\ccastillo <carlos.castillo2@soprasteria.com>
2022-09-29 12:13:33 +0200

    Init commit

---
+++ src/main/java/com/company/Calulator/CalulatorApplication.java
@@ -0,0 +1,13 @@
+package com.company.Calulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalulatorApplication.class, args);
+	}
+
+}
