fd530b0bdc14c450ccf546074106319554e7e2a9
EMEAAD\ccastillo <carlos.castillo2@soprasteria.com>
2022-09-29 12:40:32 +0200

    Creation of the Controller component code

--- src/main/java/com/company/Calulator/Controller/MainController.java
+++ src/main/java/com/company/Calulator/Controller/MainController.java
@@ -1,4 +1,27 @@
 package com.company.Calulator.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping(name="/")
 public class MainController {
+
+    @Autowired
+    private ResultServiceImpl resultServiceImpl;
+
+    @GetMapping(value = "/Sum/operator_1/{operator_1}/operador_2/{operador_2}")
+    public ResultObject Sum (@PathVariable(value = "operator_1") BigDecimal operator_1, @PathVariable(value = "operator_2") BigDecimal operator_2){
+        return resultServiceImpl.sum(operator_1, operator_2);
+    }
+
+    @GetMapping(value = "Subtract/operator_1/{operator_1}/operator_2/{operator_2}")
+    public ResultObject Subtract (@PathVariable(value = "operator_1") BigDecimal operator_1, @PathVariable (value = "operator_2") BigDecimal operator_2){
+        return resultServiceImpl.subtract(operator_1, operator_2);
+    }
 }
